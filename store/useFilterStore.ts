88eeff647152e8583d721dd34fe8e3d69fdb8860
Shanto <abdurrahman19011@gmail.com>
2025-06-23 00:42:55 +0600

    Updated Changes

--- store/useFilterStore.ts
+++ store/useFilterStore.ts
@@ -1,4 +1,14 @@
-import create from "zustand";
+import { create } from "zustand";
+
+interface FilterState {
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
+}
+
+export const useFilterStore = create<FilterState>((set) => ({
+  searchTerm: "",
+  setSearchTerm: (term) => set({ searchTerm: term }),
+}));
 
 interface FilterState {
   searchTerm: string;
