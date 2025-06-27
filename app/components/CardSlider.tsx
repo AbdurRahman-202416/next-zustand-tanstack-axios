9c658c816aef7c0b9c46f3d014a38de39978268c
Md Abdur Rahman <rahman.abdur@konasl.com>
2025-06-27 12:41:17 +0600

    add

--- app/components/CardSlider.tsx
+++ app/components/CardSlider.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Swiper as SwiperCore } from "swiper/types";
 
-
 import "swiper/css";
 import Image from "next/image";
 
@@ -11,16 +10,19 @@ const cards = [
     id: 1,
     image: "/card/_card.png",
     name: "현대카드 오리지널 CT ... (1234)",
+    isValid: false,
   },
   {
     id: 2,
     image: "/card/card-2.png",
     name: "신한카드 트래블제로 ... (5678)",
+    isValid: true,
   },
   {
     id: 3,
     image: "/card/_card.png",
     name: "현대카드 오리지널 CT",
+    isValid: true,
   },
 ];
 
@@ -43,18 +45,33 @@ export default function CardSlider() {
         {cards.map((card, index) => (
           <SwiperSlide key={index}>
             <div
-              className={` text-center transition-all duration-300 ${
+              className={`relative text-center transition-all duration-300 ${
                 currentIndex - 1 === index
                   ? "opacity-100 scale-100"
                   : "opacity-50 scale-90"
               }`}
             >
+             
+              {!card.isValid && (
+                <div className="absolute inset-0 z-50 flex items-center justify-center">
+                  <Image
+                    src="/card/tip.png"
+                    alt="Tip Icon"
+                    width={40}
+                    height={40}
+                   
+                  />
+                </div>
+              )}
+
+              {/* Card Image */}
               <Image
                 src={card.image}
                 alt={`Card ${card.id}`}
                 width={101}
                 height={159}
-                className="w-full rounded-[8px] object-cover mx-auto"
+                className={`w-full rounded-[8px] z-30 object-cover mx-auto ${
+                  card.isValid ? "opacity-100" : "opacity-30 brightness-50"}`}
               />
             </div>
           </SwiperSlide>
@@ -78,12 +95,12 @@ export default function CardSlider() {
       {/* Bottom Info */}
       <div className="mt-4 w-full h-[30px] px-4 items-center text-center flex flex-row justify-between text-sm">
         <p className="text-gray-500">충전잔액</p>
-       <div className="flex items-center gap-3 justify-center">
-         <p className="font-bold text-lg">{"{5,000,000}"}원</p>
-        <button className="ml-2 px-3 py-1 text-xs bg-blue-500 text-white rounded">
-          충전
-        </button>
-       </div>
+        <div className="flex items-center gap-3 justify-center">
+          <p className="font-bold text-lg">{"{5,000,000}"}원</p>
+          <button className="ml-2 px-3 py-1 text-xs bg-blue-500 text-white rounded">
+            충전
+          </button>
+        </div>
       </div>
     </div>
   );
